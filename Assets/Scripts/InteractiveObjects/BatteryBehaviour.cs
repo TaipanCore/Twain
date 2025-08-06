6dbe286c351ea9f07d4ddfc8b90201bb95de6772
Taipania <artpost05@bk.ru>
2025-08-06 23:41:05 +0300

    Анимации батарейки

--- Assets/Scripts/InteractiveObjects/BatteryBehaviour.cs
+++ Assets/Scripts/InteractiveObjects/BatteryBehaviour.cs
@@ -1,47 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+
 
 public class BatteryBehaviour : MonoBehaviour
 {
-    [SerializeField] private Sprite[] batterySprites;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float chargeDisplayTime = 0.5f;
+    [SerializeField] private float chargeHideTime = 1f;
+    private WaitForSeconds _shortDelay;
+    private WaitForSeconds _longDelay;
+    private Coroutine _blinking;
 
-    [HideInInspector] public bool isCharging;
+    [SerializeField] private Sprite[] _batterySprites;
+    private Sprite _currentSprite;
+    private SpriteRenderer _spriteRenderer;
+
+    private bool _isCharging;
+    [HideInInspector] public bool isCharging
+    {
+        get => _isCharging;
+        set
+        {
+            _isCharging = value;
+            if (_isCharging)
+            {
+                if (_blinking == null)
+                    _blinking = StartCoroutine(SpriteBlinking());
+            }               
+            else
+            {
+                if (_blinking != null)
+                {
+                    StopCoroutine(_blinking);
+                    _blinking = null;                  
+                }
+                _spriteRenderer.sprite = _currentSprite;
+            }
+        }
+    }
     private float _batteryCharge;
     [HideInInspector] public float batteryCharge
     {
-        get
-        {
-            return _batteryCharge;
-        }
+        get => _batteryCharge;
         set
         {
             _batteryCharge = Mathf.Clamp(value, 0f, 100f);
-            Debug.Log($"{Mathf.FloorToInt(_batteryCharge)}%");            
+            Debug.Log($"{Mathf.FloorToInt(_batteryCharge)}%");
         }
     }
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _shortDelay = new WaitForSeconds(chargeDisplayTime);
+        _longDelay = new WaitForSeconds(chargeHideTime);
+        _currentSprite = _batterySprites[0];
     }
     private void Update()
     {
-        if (isCharging)
+        if (_isCharging)
         {
-            if (_batteryCharge < 25f)
-                spriteRenderer.sprite = batterySprites[0];
-            else if (_batteryCharge < 50f)
-                spriteRenderer.sprite = batterySprites[1];
-            else if (_batteryCharge < 75f)
-                spriteRenderer.sprite = batterySprites[2];
-            else if (_batteryCharge < 100f)
-                spriteRenderer.sprite = batterySprites[3];
-            else
-            {
-                spriteRenderer.sprite = batterySprites[4];
+            _currentSprite = _batterySprites[Mathf.FloorToInt(_batteryCharge / 25f)];
+            if (_currentSprite == _batterySprites[_batterySprites.Length - 1])
                 isCharging = false;
-            }               
+        }
+    }
+    private IEnumerator SpriteBlinking()
+    {
+        while (true)
+        {
+            _spriteRenderer.sprite = _currentSprite;
+            yield return _shortDelay;
+            _spriteRenderer.sprite = _batterySprites[0];
+            yield return _longDelay;      
         }
     }
 }
