7fb3d90dd51a233c7b57f5a3de7aea13042508b9
TaipanCore <artpost05@bk.ru>
2026-02-19 21:13:42 +0300

    Добавлен спавн частиц у шипов темной стороны

--- Assets/UnityGame/Scripts/Player/DarkSide/SpikesSpawner.cs
+++ Assets/UnityGame/Scripts/Player/DarkSide/SpikesSpawner.cs
@@ -1,14 +1,21 @@
 using System.Collections;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class SpikesSpawner : MonoBehaviour
 {
+    [Header("Spikes")]
     [SerializeField] private GameObject spikePrefab;
     [SerializeField] float stepBetweenSpikes;
     [SerializeField, Min(0)] private float spawnPositionOffset;
     [SerializeField, Min(0)] float spikesSpawnTimeInterval;
     private WaitForSeconds spikesSpawnDelay;
+
+    [Header("Particles")]
+    [SerializeField] private ParticleSystem particlesPrefab;
     
     private int numberOfSpikes;
     float spikesLifetime;
@@ -16,6 +23,7 @@ public class SpikesSpawner : MonoBehaviour
     private Transform objectTransform;
     private Vector3 direction;
     private Vector3 startPosition;
+    private Vector3 endPosition;
     private Tilemap tilemap;
     
     void Start()
@@ -26,7 +34,7 @@ public class SpikesSpawner : MonoBehaviour
         StartCoroutine(SpawnSpikes());
     }
 
-    void LateUpdate()
+    void Update()
     {
         if (objectTransform.childCount == 0)
         {
@@ -37,6 +45,8 @@ public class SpikesSpawner : MonoBehaviour
     {
         direction = (MouseTracker.mousePosition - transform.position).normalized;
         startPosition = transform.position + direction * spawnPositionOffset;
+        endPosition = startPosition + (numberOfSpikes - 1) * stepBetweenSpikes * direction;
+        SpawnParticles();
         for (int i = 0; i < numberOfSpikes; i++)
         {
             Vector3 position = startPosition + i * stepBetweenSpikes * direction;
@@ -49,15 +59,25 @@ public class SpikesSpawner : MonoBehaviour
             yield return spikesSpawnDelay;
         }
     }
-
+    
     public void Initialize(int numberOfSpikes, float spikesLifetime)
     {
         this.numberOfSpikes = numberOfSpikes;
         this.spikesLifetime = spikesLifetime;
     }
+
+    private void SpawnParticles()
+    {
+        ParticleSystem particles = Instantiate(particlesPrefab, (endPosition + startPosition) / 2f, Quaternion.FromToRotation(Vector3.up, endPosition - startPosition), objectTransform);
+        ParticleSystem.MainModule main = particles.main;
+        main.duration =  spikesLifetime;
+        ParticleSystem.ShapeModule shape = particles.shape;
+        shape.scale = new Vector3(shape.scale.x, numberOfSpikes * stepBetweenSpikes, shape.scale.z);
+        particles.Play();
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(startPosition, startPosition + (numberOfSpikes - 1) * stepBetweenSpikes * direction);
+        Gizmos.DrawLine(startPosition, endPosition);
     }
 }
