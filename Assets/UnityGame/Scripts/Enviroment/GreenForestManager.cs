65638e54533da026483d02932e2ee3d49fdfe002
Taipania <artpost05@bk.ru>
2026-04-30 23:06:23 +0300

    Добавлен спавн светлячка-путешественника для сопровождения в зеленый лес

--- Assets/UnityGame/Scripts/Enviroment/GreenForestManager.cs
+++ Assets/UnityGame/Scripts/Enviroment/GreenForestManager.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class GreenForestManager : MonoBehaviour
 {
     [SerializeField, Min(0)] private float delayBetweenFirefliesSpawn;
+    private WaitForSeconds firefliesSpawnTimer;
+    [SerializeField, Min(0)] private float delayBetweenTravelerFirefliesSpawn;
+    private WaitForSeconds travelerFireflySpawnTimer;
     [SerializeField, Min(0)] private float fireflyLifetime;
     [SerializeField] private Transform[] fireflySpawnPoints;
+    [SerializeField] private Transform travelerFireflySpawnPoint;
     
-    private GameObjectsPool fireflyPool;
+    private GameObjectsPool blueFireflyPool;
+    private GameObjectsPool greenFireflyPool;
     private bool playerInForest;
-    private Coroutine fireflyCoroutine;
     
     void Start()
     {
-        fireflyPool = GameObject.Find("FireflyPool").GetComponent<GameObjectsPool>();
+        blueFireflyPool = GameObject.Find("BlueFireflyPool").GetComponent<GameObjectsPool>();
+        greenFireflyPool = GameObject.Find("GreenFireflyPool").GetComponent<GameObjectsPool>();
+        firefliesSpawnTimer = new WaitForSeconds(delayBetweenFirefliesSpawn);
+        travelerFireflySpawnTimer = new WaitForSeconds(delayBetweenTravelerFirefliesSpawn);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out DarkSideBehaviour _))
         {
             playerInForest = true;
-            fireflyCoroutine ??= StartCoroutine(SpawnFireflies());
+            StartCoroutine(SpawnFireflies());
+            StartCoroutine(SpawnTravelerFirefly());
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -36,13 +44,25 @@ public class GreenForestManager : MonoBehaviour
     {
         while (playerInForest)
         {
-            GameObject firefly = fireflyPool.Get((GameObject _) => { });
             Transform[] startAndEndPoints = Utils.GetRandomElements(fireflySpawnPoints, 2);
-            firefly.transform.position = startAndEndPoints[0].position;
-            firefly.GetComponent<FireflyMovement>().MoveAlongPath(startAndEndPoints[1].position, fireflyLifetime).OnComplete(() => fireflyPool.Return(firefly, (GameObject _) => { }));
-            yield return new WaitForSeconds(delayBetweenFirefliesSpawn);
+            FireflyLifeCycle(startAndEndPoints[0].position, startAndEndPoints[1].position, fireflyLifetime);
+            yield return firefliesSpawnTimer;
         }
-        fireflyCoroutine = null;
+    }
+    private IEnumerator SpawnTravelerFirefly()
+    {
+        while (playerInForest)
+        {
+            FireflyLifeCycle(travelerFireflySpawnPoint.position, fireflySpawnPoints[0].position, fireflyLifetime);
+            yield return travelerFireflySpawnTimer;
+        }
+    }
+    private void FireflyLifeCycle(Vector3 startPosition, Vector3 endPosition, float duration)
+    {
+        GameObjectsPool pool = Random.Range(0, 2) == 0 ? blueFireflyPool : greenFireflyPool;
+        GameObject firefly = pool.Get(_ => { });
+        firefly.transform.position = startPosition;
+        firefly.GetComponent<FireflyMovement>().MoveAlongPath(endPosition, duration).OnComplete(() => pool.Return(firefly, _ => { }));
     }
 }
 
